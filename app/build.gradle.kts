aca18487b412de77a45935698f5f98900f67aaf7
atarusov <tvinkvinter@gmail.com>
2025-12-04 16:55:46 +0400

    bump version code to 5 and version name to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.atarusov.justcounter"
         minSdk = 28
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
