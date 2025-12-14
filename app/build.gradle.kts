be6251237022207612329b610ed382358a6a46d8
atarusov <tvinkvinter@gmail.com>
2025-12-14 19:50:44 +0400

    build: bump version code to 6 and version name to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.atarusov.justcounter"
         minSdk = 28
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1.0"
+        versionCode = 6
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
