70ba7f88aee5656fcb71868a4935aa0a1476e879
atarusov <tvinkvinter@gmail.com>
2026-04-05 18:17:24 +0400

    build: bump version code to 8 and version name to 1.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.atarusov.justcounter"
         minSdk = 28
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.2.1"
+        versionCode = 8
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
